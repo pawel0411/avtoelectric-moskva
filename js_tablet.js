65e3b1d2bececbaa628eb852c30edc50d002d562
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2022-03-08 11:29:53 +0200

    Add files via upload

---
+++ js_tablet.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.slider-tablet ul').bxSlider({
+		pagerCustom: '.slider_pager-tablet ul',
+		controls: true,  
+		auto: false,
+		minSlides: 1,
+		maxSlides: 1
+	});
+});
---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.slider ul').bxSlider({
+		pagerCustom: '.slider_pager ul',
+		controls: true,  
+		auto: false,
+		minSlides: 1,
+		maxSlides: 1
+	});
+});
